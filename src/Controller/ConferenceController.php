e464de26128599d0b2c43977aae600990f4f08ee
Laurent Segura <54800835+luowense@users.noreply.github.com>
2020-03-31 15:22:37 +0200

    chapitre 10.7

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -7,35 +7,59 @@ use App\Entity\Conference;
 use App\Repository\CommentRepository;
 use App\Repository\ConferenceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
 
 class ConferenceController extends AbstractController
 {
+    private $twig;
+
+    public function __construct(Environment $twig)
+    {
+        $this->twig = $twig;
+    }
+
     /**
      * @Route("/", name="homepage")
-     * @param Environment $twig
      * @param ConferenceRepository $conferenceRepository
      * @return Response
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
      */
-    public function index(Environment $twig, ConferenceRepository $conferenceRepository)
+    public function index(ConferenceRepository $conferenceRepository)
     {
-        return new Response($this->render('conference/index.html.twig',
-            ['conferences' => $conferenceRepository->findAll()]
-            ));
+        return new Response($this->twig->render('conference/index.html.twig', [
+            'conferences' => $conferenceRepository->findAll()
+            ]
+        ));
     }
 
     /**
      * @Route("/conference/{id}", name="conference")
+     * @param Request $request
+     * @param Conference $conference
+     * @param CommentRepository $commentRepository
+     * @return Response
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
      */
-    public function show(Environment $twig, Conference $conference, CommentRepository $commentRepository)
+    public function show(Request $request, Conference $conference, CommentRepository $commentRepository)
     {
-        return new Response($twig->render('conference/show.html.twig',[
-            'conference' => $conference,
-                'comments' => $commentRepository->findBy(
-                    ['conference' => $conference],
-                    ['createdAt' => 'DESC'])
+        $offset = max(0, $request->query->getInt('offset', 0));
+        $paginator = $commentRepository->getCommentPaginator($conference, $offset);
+
+        return new Response($this->twig->render('conference/show.html.twig', [
+                'conference' => $conference,
+                'comments' => $paginator,
+                'previous' => $offset - CommentRepository::PAGINATOR_PER_AGE,
+                'next' => min(count($paginator), $offset + CommentRepository::PAGINATOR_PER_AGE)
             ]
         ));
     }
